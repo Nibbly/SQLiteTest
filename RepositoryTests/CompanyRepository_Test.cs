1c022886b4ea234ef740e07ba2deafc5925eadc6
Nibbly <34423578+Nibbly@users.noreply.github.com>
2019-12-14 15:26:53 +0100

    more changes

--- RepositoryTests/CompanyRepository_Test.cs
+++ RepositoryTests/CompanyRepository_Test.cs
@@ -14,39 +14,69 @@ namespace RepositoryTests
 
     public class CompanyRepository_Test
     {
+        private DBConnection _dbConnectionCompany;
+        private DBConnection _dbConnectionAddress;
+        private SQLiteConnection _sqliteConnectionCompany;
+        private SQLiteConnection _sqliteConnectionAddress;
+        private SQLiteDataReader _sqliteDatareader;
+        private SQLiteCommand sqlite_cmd;
+
+        public CompanyRepository_Test()
+        {
+            _dbConnectionCompany = HelperMethods.GetConnection();
+            _dbConnectionAddress = HelperMethods.GetConnection();
+        }
+
         [Fact]
         public void ShouldAddCompanyThatIsNotYetInDatabase()
         {
-            SQLiteConnection sqlite_connCompanyDB;
-            DBConnection connectionCompanyDB = new DBConnection();
-            sqlite_connCompanyDB = connectionCompanyDB.CreateConnection(DatabaseNames.COMPANY_DB_NAME);
-
-            SQLiteConnection sqlite_connAddressDB;
-            DBConnection connectionAddressDB = new DBConnection();
-            sqlite_connAddressDB = connectionAddressDB.CreateConnection(DatabaseNames.ADDRESS_DB_NAME);
-
-            AddressRepository aRep = new AddressRepository(sqlite_connAddressDB);
-            CompanyRepository crep = new CompanyRepository(sqlite_connCompanyDB, aRep);
-
-            Company company = GetCompany();
-            crep.Add(company);
-
-            SQLiteDataReader sqlite_datareader;
-            SQLiteCommand sqlite_cmd;
-            sqlite_cmd = sqlite_connCompanyDB.CreateCommand();
-            sqlite_cmd.CommandText = $"SELECT Id FROM Company WHERE Name = '{company.Name}' AND Address = '{aRep.GetId(company.Address)}' AND JuristicalNature = '{(int)company.JuristicalNature}' AND UseFrequency = '{(int)company.UseFrequency}'";
-            sqlite_datareader = sqlite_cmd.ExecuteReader();
-
-            while (sqlite_datareader.Read())
+            using (_sqliteConnectionCompany = HelperMethods.CreateConnection(DatabaseNames.COMPANY_DB_NAME))
             {
-                company.Id = sqlite_datareader.GetInt32(0);
+                using (_sqliteConnectionAddress = HelperMethods.CreateConnection(DatabaseNames.ADDRESS_DB_NAME))
+                {
+                    AddressRepository aRep = new AddressRepository(_sqliteConnectionAddress);
+                    CompanyRepository crep = new CompanyRepository(_sqliteConnectionCompany, aRep);
+
+                    Company company = GetCompany();
+                    crep.Add(company);
+
+                    sqlite_cmd = _sqliteConnectionCompany.CreateCommand();
+                    sqlite_cmd.CommandText = $"SELECT Id FROM Company WHERE Name = '{company.Name}' AND Address = '{aRep.GetId(company.Address)}' AND JuristicalNature = '{(int)company.JuristicalNature}' AND UseFrequency = '{(int)company.UseFrequency}'";
+                    _sqliteDatareader = sqlite_cmd.ExecuteReader();
+
+                    while (_sqliteDatareader.Read())
+                    {
+                        company.Id = _sqliteDatareader.GetInt32(0);
+                    }
+
+                    int numOfRows = crep.GetAll().Count;
+                    crep.Delete(company);
+                    int numOfRowsNew = crep.GetAll().Count;
+
+                    Assert.Equal(numOfRows - 1, numOfRowsNew);
+                }
             }
+        }
 
-            int numOfRows = crep.GetAll().Count;
-            crep.Delete(company);
-            int numOfRowsNew = crep.GetAll().Count;
+        [Fact]
+        public void ShouldNotAddCompanyThatIsAlreadyInDatabase()
+        {
+            using (_sqliteConnectionCompany = HelperMethods.CreateConnection(DatabaseNames.COMPANY_DB_NAME))
+            {
+                using (_sqliteConnectionAddress = HelperMethods.CreateConnection(DatabaseNames.ADDRESS_DB_NAME))
+                {
+                    Company comp1 = GetCompany();
+                    CompanyRepository cRep = new CompanyRepository(_sqliteConnectionCompany, new AddressRepository(_sqliteConnectionAddress));
 
-            Assert.Equal(numOfRows - 1, numOfRowsNew);
+                    cRep.Add(comp1);
+
+                    int numOfRows = cRep.GetAll().Count;
+                    cRep.Add(comp1);
+                    int newNumOfRows = cRep.GetAll().Count;
+
+                    Assert.Equal(numOfRows, newNumOfRows);
+                }
+            }
         }
 
         private static Address GetAddress()
---
+++ SQLite/HelperMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLite
+{
+    public static class HelperMethods
+    {
+        public static DBConnection GetConnection()
+        {
+            return new DBConnection();
+        }
+
+        public static SQLiteConnection CreateConnection(string dbName)
+        {
+            string databaseName = dbName;
+
+            SQLiteConnection sqlite_conn;
+            // Create a new database connection:
+            sqlite_conn = new SQLiteConnection($"Data Source={databaseName}; Version = 3; New = True; Compress = True; ");
+            // Open the connection:
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+                return new SQLiteConnection();
+            }
+            return sqlite_conn;
+        }
+    }
+}
